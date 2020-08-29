25a8ec200bcf933eb9bbaedac1f562d8940f139b
abhishekchand819 <abhishekchand055@gmail.com>
2020-08-29 17:27:35 +0530

    commit

--- src/components/shared/Trailer/index.js
+++ src/components/shared/Trailer/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-// import React, { useState, useEffect } from "react";
+// import React from "react";
+import React, { useState, useEffect } from "react";
 
-// import axios from "axios";
+import axios from "axios";
 import ReactPlayer from "react-player";
 
 export default function Trailer({ movie }) {
@@ -16,25 +16,25 @@ export default function Trailer({ movie }) {
     width = "1200px";
     height = "704px";
   }
-  // const [link, setLink] = useState("");
-  //   useEffect(() => {
-  //     axios
-  //       .get("https://www.googleapis.com/youtube/v3/search?", {
-  //         params: {
-  //           part: "snippet",
-  //           q: `${movie} trailer`,
-  //           key: "AIzaSyC6GzKceImAbeDlV2HHwrt3bD9ID5h6_Yc",
-  //         },
-  //       })
-  //       .then((response) => {
-  //         setLink(response.data.items[0].id["videoId"]);
-  //       });
-  //   }, []);
+  const [link, setLink] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://www.googleapis.com/youtube/v3/search?", {
+        params: {
+          part: "snippet",
+          q: `${movie} trailer`,
+          key: "AIzaSyC6GzKceImAbeDlV2HHwrt3bD9ID5h6_Yc",
+        },
+      })
+      .then((response) => {
+        setLink(response.data.items[0].id["videoId"]);
+      });
+  }, [movie]);
   return (
     <ReactPlayer
-      //   url={`https://youtu.be/${link}`}
+      url={`https://youtu.be/${link}`}
       //   url="https://youtu.be/ItXGXPxy03o"
-      url="https://youtu.be/luai0p0y2zE"
+      // url="https://youtu.be/luai0p0y2zE"
       controls
       playbackRate={1}
       width={width}
